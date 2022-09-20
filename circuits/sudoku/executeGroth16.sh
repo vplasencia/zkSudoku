3b18c3329251c03fa2833a69ac67d30592f99de1
vplasencia <v.pcalana@gmail.com>
2022-09-20 15:44:14 -0400

    Update PTAU variable

--- circuits/sudoku/executeGroth16.sh
+++ circuits/sudoku/executeGroth16.sh
@@ -13,7 +13,7 @@ fi
 
 # In case there is a ptau file number as an input
 if [ "$2" ]; then
-    CIRCUIT=$2
+    PTAU=$2
 fi
 
 # Check if the necessary ptau file already exists. If it does not exist, it will be downloaded from the data center
